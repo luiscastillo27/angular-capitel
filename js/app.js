957b28f570f606be676dd8318a449d239b5681c2
luiscastillo27 <luis@AirLuis.local>
2017-07-10 12:30:40 -0500

    Principal

---
+++ js/app.js
@@ -0,0 +1,36 @@
+(function(){
+
+  var appFont = angular.module('jimApp', ['ngRoute', 'InicioCtrl', 
+  'HistoriaCtrl', 'ProductoCtrl', 'ContactoCtrl' ,'BuscarCtrl', 'LoginCtrl']);
+
+  appFont.config(function($routeProvider) {
+        $routeProvider.when('/', {
+          controller: 'InicioCtrl',
+          templateUrl: 'templates/inicio.html'
+        })
+        $routeProvider.when('/historia', {
+          controller: 'HistoriaCtrl',
+          templateUrl: 'templates/historia.html'
+        })
+        $routeProvider.when('/producto', {
+          controller: 'ProductoCtrl',
+          templateUrl: 'templates/producto.html'
+        })
+        $routeProvider.when('/contacto', {//
+          controller: 'ContactoCtrl',
+          templateUrl: 'templates/contacto.html'
+        })
+        $routeProvider.when('/buscar', {
+          controller: 'BuscarCtrl',
+          templateUrl: 'templates/buscar.html'
+        })
+        $routeProvider.when('/login', {
+          controller: 'LoginCtrl',
+          templateUrl: 'templates/login.html'
+        })
+        $routeProvider.otherwise({ 
+          redirectTo: '/'
+        });
+    });
+
+}());
